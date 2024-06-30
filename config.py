9b4d48cdd0bc1128dafa087a446d6a7255399e86
Shutofsky <Shutofsky@gmail.com>
2024-06-30 12:49:24 +0300

    modified:   config.py modified:   templates/system_config.yml.template

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
 from skabenclient.config import DeviceConfigExtended
 
 ESSENTIAL = {
-        'minimal': 'boilerplate'        
+        'is_power_on': False,
+        'is_locked' : False,
+        'is_hacked' : False,
+        'lock_timeout' : 30,
+        'word_length' : 8,
+        'words_printed' : 16,
+        'attempts' : 4,
+        'start_header' : 'startScreen.txt',
+        'hack_header' : 'hackScreen.txt',
+        'unpower_header' : 'powerScreen.txt',
+        'lock_header' : 'lockScreen.txt',
+        'main_header' : 'readScreen.txt',
+        'menu_header' : 'menuScreen.txt',
+        'text_menu' : {'Водки лётчикам не давать': {'type':'text','name':'f3Doc.txt'},}
 }
 
 # DeviceConfigExtended supports file and json download and management
