7495a175b8405847006355114bc72dea86388828
Demetri Bis <26793601+zerthmonk@users.noreply.github.com>
2024-08-20 18:12:18 +0200

    rm: redundancy in config

--- config.py
+++ config.py
@@ -19,19 +19,8 @@ ESSENTIAL = {
     },
 }
 
-# DeviceConfigExtended supports file and json download and management
-# use DeviceConfig if you don't need this functionality
-
 
 class CursesConfig(DeviceConfigExtended):
     def __init__(self, config):
         self.minimal_essential_conf = ESSENTIAL
         super().__init__(config)
-
-    def save(self, data=None):
-        # <-- add here your specific config serializing
-        return super().save()
-
-    def load(self):
-        # <-- add here your specific config deserializing
-        return super().load()
